f287089b3f16a49e228d6d7fef75dd14bf8352f7
IMP1 <huw_taylor@hotmail.co.uk>
2015-01-28 13:59:17 +0000

    added password field to user

--- models/db.py
+++ models/db.py
@@ -2,4 +2,5 @@ db = DAL('sqlite://store.db')
 
 db.define_table('user',
                     Field('username', 'string', requires=[IS_NOT_EMPTY(), IS_NOT_IN_DB(db, 'user.username')]),
+                    Field('password', 'password', requires=[IS_NOT_EMPTY()]),
                )
