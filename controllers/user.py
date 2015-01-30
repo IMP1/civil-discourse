22a4b27d71eb6f0d6d01f313bb6085d1d6e4a04f
IMP1 <huw_taylor@hotmail.co.uk>
2015-01-30 16:25:25 +0000

    created new login form and handle wrong passwords

--- controllers/user.py
+++ controllers/user.py
@@ -3,12 +3,17 @@
 # 
 def login():
     # Create the form.
-    form = SQLFORM.factory(db.user)
+    form = FORM(
+                INPUT(_name='username', requires=IS_IN_DB(db, 'user.username')),
+                INPUT(_name='password', requires=IS_NOT_EMPTY()),
+                INPUT(_type='submit')
+               )
     # If the form is valid:
-    if form.validate() and db(db.user.username == form.vars.username).select():
-            # Log the user in.
-            user = db(db.user.username == form.vars.username).select(db.user.id)[0]
-            session.logged_in_user = user.id
+    if form.validate():
+        # Log the user in.
+        user = db((db.user.username == form.vars.username) & (db.user.password == form.vars.password)).select(db.user.id)
+        if user:
+            session.logged_in_user = user[0].id
             # Let them know they've been successful.
             session.flash = "You've successfully logged in!"
             # If we were going somewhere:
@@ -20,6 +25,8 @@ def login():
             else:
                 # Otherwise go to the homepage.
                 redirect(URL('default', 'index.html'))
+        else:
+            form.errors.password = "Incorrect password"
     return dict(form=form)
 
 # 
