4122315db04bf44e5fb4380f87e1cd517499977f
IMP1 <IMP1@mail.com>
2015-04-14 18:36:14 +0100

    added tags to conversations

--- models/db.py
+++ models/db.py
@@ -5,6 +5,11 @@ db.define_table('user',
                     Field('password', 'password', requires=[IS_NOT_EMPTY(error_message='The password cannot be empty')]),
                )
 
+db.define_table('tag',
+                    Field('name', 'string', requires=[IS_NOT_EMPTY(error_message='The tag name cannot be empty.'), IS_NOT_IN_DB(db, 'tag.name', error_message='Sorry! This tag already exists.')]),
+                    Field('scope', 'string', requires=[IS_IN_SET(('user', 'site'))]),
+               )
+
 # Groups messages
 db.define_table('conversation',
                     Field('title', 'string', requires=[IS_NOT_EMPTY(error_message='You must provide a discussion title.')]),
@@ -14,6 +19,11 @@ db.define_table('message',
                     Field('author', db.user),
                     Field('conversation', db.conversation),
                     Field('contents', 'text', requires=[IS_NOT_EMPTY(error_message='You haven\'t written a message.')]),
-                    Field('time', 'datetime')
+                    Field('time', 'datetime'),
                 )
 
+
+db.define_table('conversation_tag',
+                    Field('conversation', db.conversation),
+                    Field('tag', db.tag,)
+               )
