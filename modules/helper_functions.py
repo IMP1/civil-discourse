0cd270991f1d244cac091e9997668c9b45d5f19f
IMP1 <IMP1@mail.com>
2015-10-29 23:43:47 +0000

    added search which highlights results

--- modules/helper_functions.py
+++ modules/helper_functions.py
@@ -1,6 +1,9 @@
 from gluon import current
+from gluon.html import CAT, SPAN
 T = current.T
 import datetime
+import re
+from message import *
 
 def getRecentMessages(db, count):
     return db(db.message.id > 0).select(orderby=~db.message.time, limitby=(0, count))
@@ -14,7 +17,7 @@ def getLastMessageTime(db, discussion_id):
     delta_time = time_now - last_time
     elapsed_days = delta_time.days
     if elapsed_days < 0:
-        return "in the future somehow"
+        return T("in the future somehow")
     if elapsed_days == 0:
         elapsed_seconds = delta_time.seconds
         if elapsed_seconds < 5:
@@ -35,6 +38,16 @@ def getLastMessageTime(db, discussion_id):
         return T("%s %%{month} ago", symbols=(elapsed_days / 30))
     return T("%s %%{year} ago", symbols=(elapsed_days / 365))
 
+def getMessageCount(db, discussion_id):
+    return db(db.message.conversation == discussion_id).count()
+
+def getMessages(db, discussion_id, oldest_first=True):
+    if oldest_first:
+        order = db.message.time
+    else:
+        order = ~db.message.time
+    return db(db.message.conversation == discussion_id).select(orderby=order)
+
 def hasUserContributed(db, discussion_id, user_id):
     return db((db.message.conversation == discussion_id) & (db.message.author == user_id)).count() > 0
 
@@ -49,3 +62,21 @@ def getSiteTags(db, discussion_id):
 
 def getUserTags(db, discussion_id):
     return db((db.conversation_tag.conversation == discussion_id) & (db.conversation_tag.tag == db.tag.id) & (db.tag.scope == 'user')).select(db.tag.ALL)
+
+def getDiscussionSearchResults(db, search_query):
+    query = db.conversation.title.contains(search_query)
+    # Add extra searches here
+    return db(query).select(db.conversation.ALL)
+
+def getMessageSearchResults(db, search_query):
+    query = db.message.contents.contains(search_query)
+    # Add extra searches here
+    return db(query).select(db.message.ALL)
+
+def highlightWords(text, words):
+    result = ""
+    if type(words) is str:
+        begin = re.search(words, text, re.IGNORECASE).start()
+        end = begin + len(words)
+        return CAT(text[:begin], SPAN(text[begin:end], _class="highlighted"), text[end:])
+    # Add an alternative for a list of words
