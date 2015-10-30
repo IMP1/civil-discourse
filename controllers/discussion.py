397409cef9ac1ddf1dfbc7a88518a64e6e4e96e6
IMP1 <IMP1@mail.com>
2015-10-30 19:45:33 +0000

    removed duplicate code, fixes #9

--- controllers/discussion.py
+++ controllers/discussion.py
@@ -28,18 +28,7 @@ def view():
     return dict(discussion_id=discussion_id, form=form)
 
 def search():
-    if not request.vars.search_text:
-        if request.vars.search_tag:
-            redirect(URL('tags'))
-        else:
-            redirect(URL('all'))
     return dict()
 
 def rules():
-    return dict()
-
-def all():
-    return dict()
-
-def tags():
     return dict()
