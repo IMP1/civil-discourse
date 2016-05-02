62a5af674585a784ebc60438fbf89314d3feb7f2
IMP1 <IMP1@mail.com>
2016-05-02 23:04:06 +0100

    added atom feed for each discussion. closes #2

--- controllers/discussion.py
+++ controllers/discussion.py
@@ -1,6 +1,32 @@
+# -*- coding: utf-8 -*-
 import datetime
 
-# -*- coding: utf-8 -*-
+def __get_feed(discussion_id):
+    discussion = db.conversation[discussion_id]
+    feed = CAT(
+        XML("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"),
+        XML("<feed xmlns=\"http://www.w3.org/2005/Atom\">\n"),
+        XML("\t<title>"), discussion.title, XML("</title>\n"),
+        XML("\t<link href=\""), URL("discussion", "view", args=[discussion_id]), XML("\" />\n"),
+        XML("\t<id>"), "", XML("</id>\n"),
+        XML("\t<updated>"), "2016-01-01T00:00:02Z", XML("</updated>\n"),
+    )
+    for comment in db(db.message.conversation == discussion_id).select():
+        print(URL("discussion", "view", args=[discussion_id], anchor="message-" + str(comment.id), scheme=True, host=True))
+        feed = CAT(feed, 
+            XML("\t<entry>\n"),
+            XML("\t\t<title>"), "Reply by User", XML("</title>\n"),
+            XML("<link href=\""), URL("discussion", "view", args=[discussion_id], anchor="message-" + str(comment.id), scheme=True, host=True), XML("\" rel=\"alternate\" />"),
+            XML("\t\t<updated>"), str(comment.time).replace(" ", "T") + "Z", XML("</updated>\n"),
+            XML("\t\t<content type=\"xhtml\"><p>"), comment.contents, XML("</p></content>\n"),
+            XML("\t\t<author>\n"),
+            XML("\t\t\t<name>"), db.user[comment.author].username, XML("</name>\n"),
+            XML("\t\t</author>\n"),
+            XML("\t</entry>\n"),
+        )
+    feed = CAT(feed, XML("</feed>"))
+    return feed
+
 def new():
     if not session.logged_in_user:
         session.redirection = URL('discussion', 'new', vars=request.vars)
@@ -20,6 +46,9 @@ def new():
 
 def view():
     discussion_id = request.args[0]
+    if (request.args[-1] == "feed"):
+        print("getting feed!")
+        return __get_feed(discussion_id)
     form = SQLFORM.factory(db.message.contents)
     if form.validate():
         message_id = db.message.insert(author=session.logged_in_user, conversation=discussion_id, contents=form.vars.contents, time=datetime.datetime.utcnow())
